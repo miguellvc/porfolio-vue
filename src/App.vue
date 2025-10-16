48fe9b5f23709d20f47e9f14e7ba2bb496908ddc
Miguel Perez <miguellrvc@gmail.com>
2025-10-16 15:22:43 -0300

    se creo el componente NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="header">
+    <!-- Barra de nevegación -->
+    <NavBar />
   </header>
-
+  <!-- contenido principal -->
   <main>
-    <TheWelcome />
+
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.header {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 2em;
+  position: fixed;
+  z-index: 1000;
+  background: var(--color-background);
+  opacity: 0.9;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref } from 'vue';
+const navegacion = ref([
+    { id: 1, nombre: 'Inicio', enlace: 'inicio' },
+    { id: 2, nombre: 'Educación', enlace: 'educacion' },
+    { id: 3, nombre: 'Habilidad', enlace: 'habilidad' },
+    { id: 4, nombre: 'Proyectos' }
+])
+</script>
+<template>
+
+    <ul class="menu">
+        <div class="logo">
+            <img src="./src/img/logo.png" alt="logo">
+        </div>
+        <ul class="navbar">
+            <li v-for="nav in navegacion" :key="nav.nombre"><a href="nav.enlace">{{ nav.nombre }}</a></li>
+        </ul>
+    </ul>
+
+</template>
+
+<style scoped>
+.menu {
+    width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+}
+
+.navbar {
+    display: flex;
+    gap: 1rem;
+    list-style: none;
+}
+
+.navbar li a {
+    text-decoration: none;
+    color: var(--title-color);
+    font-weight: 500;
+    font-size: 1.2rem;
+}
+
+.logo img {
+    width: 4rem;
+    border-radius: 20px;
+    color: var(--title-color);
+}
+
+.navbar {
+    display: flex;
+    gap: 1.5rem;
+    list-style: none;
+}
+
+.navbar li a {
+    text-decoration: none;
+    color: var(--title-color);
+    font-weight: 500;
+    transition: color 0.3s ease;
+}
+
+.navbar li a:hover {
+    color: var(--text-color);
+    border-bottom: 2px solid;
+}
+
+/* Responsive: menú */
+
+@media (max-width: 580px) {
+    .menu {
+        width: 100%;
+        justify-content: flex-start;
+        gap: 2rem;
+    }
+}
+</style>
