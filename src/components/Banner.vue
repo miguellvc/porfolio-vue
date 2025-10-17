fcb6cd5ac5638bbad0696a0452f9aa52c5544a29
Miguel Perez <miguellrvc@gmail.com>
2025-10-17 15:00:08 -0300

    se creó el componente banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+<template>
+       <section id="banner" class="banner">
+        <article class="banner__img">
+            <img src="./src/img/img-banner.jpeg" alt="foto_personal">
+        </article>
+
+        <article class="banner__content">
+            <h2 class="content__title">Hola, soy Miguel Perez.</h2>
+            <h3 class="content__sub-title">Sobre mí...</h3>
+            <p class="content__description">Soy Técnico Universitario en Informática y actualmente estoy cursando la
+                carrera de Técnico Universitario en Programación. Me gusta la tecnología, especialmente todo lo
+                relacionado con el desarrollo de software. Disfruto aprender nuevas herramientas, enfrentar nuevos
+                desafíos. Mi objetivo es seguir creciendo en el ámbito de la Informática y lograr formar parte de
+                proyectos que involucren el desarrollo de software.</p>
+        </article>
+        
+    </section>
+</template>
+
+<style scoped>
+
+</style>
